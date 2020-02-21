3a5dca9d5e7c69ace5ad7e5efc83547c715b68f3
LINDEMBERG NUNES CAVALCANTE JUNIOR <lindemberg.j@academico.ifrn.edu.br>
2020-02-21 15:47:11 -0300

    exercicio 1-Lista1

---
+++ lista 1/exercicio 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Bem-Vindo ao C#, {nome}");
+            Console.ReadLine();
+
+        }
+    }
+}
